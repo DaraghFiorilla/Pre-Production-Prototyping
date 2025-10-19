e600e31af7516f8445da0f557102f03bdee13029
XPS <daraghfiorillafpl@gmail.com>
2025-10-19 14:28:15 +0100

    Various bugfix & TMPro import

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -11,12 +11,83 @@ public class Arrow : MonoBehaviour
     private void Update()
     {
         transform.position = new Vector2(transform.position.x, transform.position.y + speed * Time.deltaTime);
-        //Debug.Log(transform.position);
+        if (topOfList && inTrigger)
+        {
+            DetectInput();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "RhythmInput") { inTrigger = true; Debug.Log(gameObject.name + " Entered trigger"); }
-        else if (collision.gameObject.tag == "DeleteArrow") { minigameManager.DeleteArrow(gameObject, arrowType); Debug.Log("Deleting " + gameObject.name); }
+        if (collision.gameObject.tag == "RhythmInput") { inTrigger = true; }
+        else if (collision.gameObject.tag == "DeleteArrow") { minigameManager.DeleteArrow(gameObject, arrowType); }
+    }
+
+    private void DetectInput()
+    {
+        int distanceResult;
+        switch (arrowType)
+        {
+            case 0: // left
+                {
+                    if (Input.GetKeyDown(KeyCode.LeftArrow))
+                    {
+                        // detect how close to the centre the arrow is
+                        // send this info to the game manager
+                        if (Mathf.Abs(transform.position.y) >= 150) { distanceResult = 0; } // this is a miss
+                        else if (Mathf.Abs(transform.position.y) >= 100) { distanceResult = 1; } // this is bad
+                        else if (Mathf.Abs(transform.position.y) >= 50) { distanceResult = 2; } // this is good
+                        else { distanceResult = 3; } // this is perfect
+                        minigameManager.GetInputResult(distanceResult);
+                        minigameManager.DeleteArrow(gameObject, arrowType);
+                    }
+                    break;
+                }
+            case 1: // down
+                {
+                    if (Input.GetKeyDown(KeyCode.DownArrow))
+                    {
+                        if (Mathf.Abs(transform.position.y) >= 150) { distanceResult = 0; } // this is a miss
+                        else if (Mathf.Abs(transform.position.y) >= 100) { distanceResult = 1; } // this is bad
+                        else if (Mathf.Abs(transform.position.y) >= 50) { distanceResult = 2; } // this is good
+                        else { distanceResult = 3; } // this is perfect
+                        minigameManager.GetInputResult(distanceResult);
+                        minigameManager.DeleteArrow(gameObject, arrowType);
+                    }
+                    break;
+                }
+            case 2: // up
+                {
+                    if (Input.GetKeyDown(KeyCode.UpArrow))
+                    {
+                        if (Mathf.Abs(transform.position.y) >= 150) { distanceResult = 0; } // this is a miss
+                        else if (Mathf.Abs(transform.position.y) >= 100) { distanceResult = 1; } // this is bad
+                        else if (Mathf.Abs(transform.position.y) >= 50) { distanceResult = 2; } // this is good
+                        else { distanceResult = 3; } // this is perfect
+                        minigameManager.GetInputResult(distanceResult);
+                        minigameManager.DeleteArrow(gameObject, arrowType);
+                    }
+                    break;
+                }
+            case 3: // right
+                {
+                    if (Input.GetKeyDown(KeyCode.RightArrow))
+                    {
+                        if (Mathf.Abs(transform.position.y) >= 150) { distanceResult = 0; } // this is a miss
+                        else if (Mathf.Abs(transform.position.y) >= 100) { distanceResult = 1; } // this is bad
+                        else if (Mathf.Abs(transform.position.y) >= 50) { distanceResult = 2; } // this is good
+                        else { distanceResult = 3; } // this is perfect
+                        minigameManager.GetInputResult(distanceResult);
+                        minigameManager.DeleteArrow(gameObject, arrowType);
+                    }    
+                    break;
+                }
+            default:
+                {
+                    // PANIC
+                    Debug.Log(gameObject + " is panicking due to incorrect arrow type int");
+                    break;
+                }
+        }
     }
 }
--- Assets/Scripts/RhythmMinigame.cs
+++ Assets/Scripts/RhythmMinigame.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using TMPro;
 
 // TESTING VERSION OF THIS SCRIPT
 // ARROWS ARE SET TO BE SPAWNED AT SET INTERVAL (TIMEBETWEENARROWS) USING A TIMER
@@ -13,18 +14,21 @@ using System.Collections.Generic;
 
 public class RhythmMinigame : MonoBehaviour
 {
-    public List<GameObject>[] noteLists = new List<GameObject>[4]; // ORDER: LEFT = 0, DOWN = 1, UP = 2, RIGHT = 3
-    [SerializeField] GameObject arrowPrefab;
+    [Header("Lists")]
+    public List<GameObject> leftNoteList; public List<GameObject> downNoteList; public List<GameObject> upNoteList; public List<GameObject> rightNoteList;
+
+    [Header("Game Variables")]
     [SerializeField] float timeBetweenArrows; // how much time to pass between spawning a new arrow
     [SerializeField] float arrowSpeed; // how fast the arrows move
-    [SerializeField] private Vector2[] spawnPos; // for some reason, this array breaks the inspector, apparently this is a known bug in this version of unity, i fixed it by going into preferences and setting the editor font to system font
-    [SerializeField] private Transform arrowParent;
-    float timer = 0;
+    public int combo;
 
-    void Start()
-    {
-        
-    }
+    [Header("Object References")]
+    [SerializeField] GameObject arrowPrefab;
+    [SerializeField] private Transform[] arrowParents; // for some reason, this array breaks the inspector, apparently this is a known bug in this version of unity, i fixed it by going into preferences and setting the editor font to system font
+    [SerializeField] private TextMeshProUGUI comboDisplay;
+    [SerializeField] private TextMeshProUGUI[] textResultPrefabs;
+
+    float timer = 0;
 
     void Update()
     {
@@ -38,35 +42,43 @@ public class RhythmMinigame : MonoBehaviour
 
     void InstantiateArrow() // spawn new arrow
     {
-        int i = Random.Range(0, 3); // decide which type of arrow to spawn (0 = left, 1 = down, 2 = up, 3 = right)
-        GameObject obj = Instantiate(arrowPrefab, arrowParent); // spawn arrow
-        obj.transform.position = spawnPos[i]; // set arrow pos depending on type
+        int i = Random.Range(0, 4); // decide which type of arrow to spawn (0 = left, 1 = down, 2 = up, 3 = right)
+        GameObject obj = Instantiate(arrowPrefab, arrowParents[i]); // spawn arrow
+        obj.transform.localPosition = new Vector2(0, -1000); // set arrow pos depending on type
         Arrow arrowScript = obj.GetComponent<Arrow>();
         arrowScript.speed = arrowSpeed;
         arrowScript.minigameManager = this;
+        arrowScript.arrowType = i;
         switch (i) // behaviours dependent on arrow type
         {
             case 0: // left
                 {
+                    leftNoteList.RemoveAll(GameObject => GameObject == null); // Remove all deleted arrow from the list before adding this one
                     obj.transform.rotation = Quaternion.Euler(0, 0, 270); // set rotation of arrow sprite; these variables assume the sprite is facing downwards by default
-                    noteLists[i].Add(obj);
-                    TrimList(i);
+                    leftNoteList.Add(obj); // add this arrow to the list
+                    TrimList(i); // this function just makes sure the list doesnt have extra elements and the objects are in order
                     break;
                 }
             case 1: // down
                 {
+                    downNoteList.RemoveAll(GameObject => GameObject == null);
                     obj.transform.rotation = Quaternion.Euler(Vector3.zero);
+                    downNoteList.Add(obj);
                     TrimList(i);
                     break;
                 }
             case 2: // up
                 {
+                    upNoteList.RemoveAll(GameObject => GameObject == null);
                     obj.transform.rotation = Quaternion.Euler(0, 0, 180);
+                    upNoteList.Add(obj);
                     break;
                 }
             case 3: // right
                 {
+                    rightNoteList.RemoveAll(GameObject => GameObject == null);
                     obj.transform.rotation = Quaternion.Euler(0, 0, 90);
+                    rightNoteList.Add(obj);
                     break;
                 }
             default:
@@ -84,9 +96,66 @@ public class RhythmMinigame : MonoBehaviour
         TrimList(listNo);
     }
 
+    public void GetInputResult(int result) // get the input result from the arrow input
+    {
+        switch (result)
+        {
+            case 0: // miss
+                {
+                    combo = 0;
+                    break;
+                }
+            case 1: // bad
+                {
+                    combo++;
+                    break;
+                }
+            case 2: // good
+                {
+                    combo++;
+                    break;
+                }
+            case 3: // perfect
+                {
+                    combo++;
+                    break;
+                }
+        }
+    }
+
     public void TrimList(int listNo)
     {
-        noteLists[listNo].TrimExcess();
-        noteLists[listNo][0].GetComponent<Arrow>().topOfList = true;
+        switch (listNo)
+        {
+            case 0: // left
+                {
+                    leftNoteList.TrimExcess();
+                    leftNoteList[0].GetComponent<Arrow>().topOfList = true;
+                    break;
+                }
+            case 1: // down
+                {
+                    downNoteList.TrimExcess();
+                    downNoteList[0].GetComponent<Arrow>().topOfList = true;
+                    break;
+                }
+            case 2: // up
+                {
+                    upNoteList.TrimExcess();
+                    upNoteList[0].GetComponent<Arrow>().topOfList = true;
+                    break;
+                }
+            case 3: // right
+                {
+                    rightNoteList.TrimExcess();
+                    rightNoteList[0].GetComponent<Arrow>().topOfList = true;
+                    break;
+                }
+            default:
+                {
+                    // PANIC
+                    break;
+                }
+        }
     }
 }
