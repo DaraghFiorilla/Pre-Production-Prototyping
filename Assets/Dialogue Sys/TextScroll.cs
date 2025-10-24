d9cfadd065ac5a68bafc6840611a467dc56abe0d
XPS <daraghfiorillafpl@gmail.com>
2025-10-24 10:14:08 +0100

    Squashed commit of the following:
    commit b011ceb0a4932abfe1a229564f00b52dfbd87009
    Author: XPS <daraghfiorillafpl@gmail.com>
    Date:   Fri Oct 24 10:12:16 2025 +0100
    
        fix
    
    commit 263ae3a63c1958a142cc64319b1fa2fa550c7590
    Author: XPS <daraghfiorillafpl@gmail.com>
    Date:   Wed Oct 22 13:40:39 2025 +0100
    
        Updated text scroll for world space
    
    commit cb0b5a3a362fb876af6e1c8c2b3158cc1a0b379e
    Author: XPS <daraghfiorillafpl@gmail.com>
    Date:   Wed Oct 22 12:15:19 2025 +0100
    
        Scrolling text

---
+++ Assets/Dialogue Sys/TextScroll.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class TextScroll : MonoBehaviour
+{
+    [SerializeField][TextArea] private string[] displayText;
+    [SerializeField] private float scrollSpeed;
+    [SerializeField] private TextMeshProUGUI textMesh;
+    private int currentChar = 0;
+    [SerializeField] private GameObject displayCanvas;
+    private GameObject player;
+    private bool playerInTrigger;
+    private Camera mainCam;
+    //[SerializeField] private GameObject displayArrow;
+
+    private void Awake()
+    {
+        mainCam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (playerInTrigger)
+        {
+            RotateCanvas();
+        }
+    }
+
+    public void SetText(string text)
+    {
+        displayText[0] = text;
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        //dialogueManager.canAdvance = false;
+        //displayArrow.SetActive(false);
+        textMesh.text = "";
+        StartCoroutine(AnimateText());
+    }
+
+    IEnumerator AnimateText()
+    {
+        for (int i = 0; i < displayText[currentChar].Length + 1; i++)
+        {
+            textMesh.text = displayText[currentChar].Substring(0, i);
+            yield return new WaitForSeconds(scrollSpeed);
+        }
+        //dialogueManager.canAdvance = true;
+        //displayArrow.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player = other.gameObject;
+            displayCanvas.SetActive(true);
+            playerInTrigger = true;
+            UpdateText();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            displayCanvas.SetActive(false);
+        }
+    }
+
+    private void RotateCanvas()
+    {
+        displayCanvas.transform.LookAt(mainCam.transform);
+    }
+}
