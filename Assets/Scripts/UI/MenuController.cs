d1c482e15392e611fe615433e26a6fdbeee0377e
Conthorp <dylanm.mcbrien@gmail.com>
2025-11-10 17:13:27 +0000

    Initial UI Implementation
    Made rudimentary pause menu function, with quit game and resume options

---
+++ Assets/Scripts/UI/MenuController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MenuController : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+
+    public bool paused;
+
+    void Start()
+    {
+
+        pauseMenu.SetActive(false);
+
+        paused = false;
+
+    }
+
+    void Update()
+    {
+
+        if (InputSystem.actions.FindAction("Pause").WasPressedThisFrame())
+        {
+
+            Pause();
+
+        }
+
+    }
+
+    public void Pause()
+    {
+
+        if (!paused)
+        {
+
+            Time.timeScale = 0;
+
+            pauseMenu.SetActive(true);
+
+            paused = true;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+        }
+
+    }
+
+    public void Resume()
+    {
+
+        if (paused)
+        {
+
+            Time.timeScale = 1;
+
+            pauseMenu.SetActive(false);
+
+            paused = false;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+        }
+
+    }
+
+    public void Quit()
+    {
+
+        Application.Quit();
+
+    }
+}
