21b3e3b2dbd4e6a95ddd780cb07edb9b88d298b5
slademore44 <123640810+slademore44@users.noreply.github.com>
2025-12-12 14:24:08 +0000

    Stockroom done

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -57,7 +57,7 @@ public class MainManager : MonoBehaviour
         }
         else if (EndRequirementsMet())
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 
