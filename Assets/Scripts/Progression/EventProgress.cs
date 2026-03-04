5ac4e888d40f86f53a370237fbb09fc0d839201f
XPS <daraghfiorillafpl@gmail.com>
2026-03-04 11:36:05 +0000

    p

--- Assets/Scripts/Progression/EventProgress.cs
+++ Assets/Scripts/Progression/EventProgress.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 using System;
 using System.Collections.Generic;
 using UnityEngine.Events;
+using Unity.VisualScripting.FullSerializer;
+using UnityEditor;
+using TMPro;
 
 public class EventProgress : MonoBehaviour
 {
+    [SerializeField] private GameObject tasklistParentObj;
+    [SerializeField] private GameObject tasklistTaskPrefab;
     public GameObject[] event2Boxes; // TEMP VAR
     public List<GameEvent> eventQueue = new();
+    public List<TasklistTask> activeTasks = new();
 
     private void Start()
     {
@@ -16,18 +22,17 @@ public class EventProgress : MonoBehaviour
     public void UpdateFlag(int id)
     {
         if (id >= eventQueue[0].endFlags.Length) Debug.LogError("ERROR id is outside range of flags array");
-        /*for (int i = 0; i < eventQueue[0].endFlags.Length; i++)
-        {
-            if (eventQueue[0].endFlags[i] == false)
-            {
-                eventQueue[0].endFlags[i] = true;
-                break;
-            }
-        }*/
-        if (eventQueue[0].endFlags[id] == false)
+        if (eventQueue[0].endFlags[id].flag == false)
         {
             Debug.Log("Setting flag true");
-            eventQueue[0].endFlags[id] = true;
+            eventQueue[0].endFlags[id].flag = true;
+            for(int i = 0; i < activeTasks.Count; i++)
+            {
+                if (activeTasks[i].flagID == id)
+                {
+                    if (activeTasks[i].taskCountTarget > 1) activeTasks[i].taskCount++;
+                }
+            }
         }
         else
         {
@@ -40,9 +45,20 @@ public class EventProgress : MonoBehaviour
     {
         Debug.Log("Checking end conditions");
         bool failed = false;
-        foreach (bool flag in eventQueue[0].endFlags)
+        /*foreach (bool flag in eventQueue[0].endFlags[].flag)
         {
             if (!flag) failed = true;
+        }*/
+
+        // 
+        for (int i = 0; i < eventQueue[0].endFlags.Length; i++)
+        {
+            if (eventQueue[0].endFlags[i].flag == false) failed = true;
+        }
+
+        for (int i = 0; i < activeTasks.Count; i++)
+        {
+
         }
 
         if (!failed)
@@ -58,6 +74,22 @@ public class EventProgress : MonoBehaviour
         eventQueue.Remove(eventQueue[0]);
         eventQueue.TrimExcess();
         eventQueue[0].startingEvent.Invoke();
+        CreateNewTasks();
+    }
+
+    public void CreateNewTasks()
+    {
+        foreach (TasklistTask task in activeTasks) Destroy(task.gameObject);
+
+        activeTasks.Clear();
+
+        foreach (TasklistTask task in eventQueue[0].addedTasks)
+        {
+            GameObject g = Instantiate(tasklistTaskPrefab, tasklistParentObj.transform);
+            TasklistTask t = g.GetComponent<TasklistTask>();
+            activeTasks.Add(t);
+            g.GetComponent<TextMeshProUGUI>().text = t.displayText;
+        }
     }
 
     public void BoxesEvent() // TEMP FUNCTION FOR TESTING - IN MAIN SCENE, THIS FUNCTION WILL BE IN A MINIGAME / OBJECT'S RESPECTIVE SCRIPT
@@ -70,6 +102,24 @@ public class EventProgress : MonoBehaviour
         public string eventName;
         public UnityEvent startingEvent;
         [TextArea(8, 20)][Tooltip("This is purely so we can keep track of which event covers what - no script uses this data")] public string eventDescription;
-        public bool[] endFlags;
+        public EndFlag[] endFlags;
+        public TasklistTask[] addedTasks;
     }
+
+    [Serializable] public struct EndFlag
+    {
+        public bool flag;
+        public bool tasklistTask;
+        public string taskName;
+    }
+
+    /*[Serializable] public struct TasklistStruct
+    {
+        public int flagID;
+        public string displayText;
+        public int taskCountTarget;
+        public int taskCount;
+        public GameObject objRef;
+        public bool complete;
+    }*/
 }
---
+++ Assets/Scripts/Progression/TasklistTask.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class TasklistTask : MonoBehaviour
+{
+    public int flagID;
+    public string displayText;
+    public int taskCountTarget;
+    public int taskCount;
+    //public GameObject objRef;
+    public bool complete;
+}
