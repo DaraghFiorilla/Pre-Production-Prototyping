b448524a9190bf783e38c1a112949d541ff2df83
slademore44 <123640810+slademore44@users.noreply.github.com>
2026-05-27 18:26:41 +0100

    Exorcising this demonic script

--- Assets/Scenes/TestScenes/TRAILER SCENES/RotateToPlayer.cs
+++ Assets/Scenes/TestScenes/TRAILER SCENES/RotateToPlayer.cs
@@ -6,6 +6,6 @@ public class RotateToPlayer : MonoBehaviour
 
     private void Update()
     {
-        transform.forward = new Vector3(transform.position.x - playerCam.transform.position.x, 0, transform.position.z - playerCam.transform.position.z);
+        //transform.forward = new Vector3(transform.position.x - playerCam.transform.position.x, 0, transform.position.z - playerCam.transform.position.z);
     }
 }
